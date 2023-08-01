0b1d2074e6832fd203210d175674e28a9b5471dd
Kirti081 <90943362+Kirti081@users.noreply.github.com>
2023-08-01 08:20:40 +0530

    Create sortColors.cpp

---
+++ sortColors.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int high=nums.size()-1;
+        int low=0,mid=0;
+
+        while(mid<=high)
+        {
+                if(nums[mid]==0)
+                {
+                    swap(nums[low],nums[mid]);
+                    low++;
+                    mid++;
+                }
+                else if(nums[mid]==2)
+                {
+                    swap(nums[high],nums[mid]);
+                    high--;
+                    if(nums[mid]==0)
+                    {
+                         swap(nums[low],nums[mid]);
+                        low++;
+                        mid++;
+                    }
+                    else if(nums[mid]==1){
+                        mid++;
+                    }
+                }
+                else{
+                    mid++;
+                }
+
+
+        }
+    }
+};
